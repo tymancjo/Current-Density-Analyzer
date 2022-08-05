569bcf8930f4f08a993bac89a2b94848209d7983
tymancjo <tomasz.tomanek@gmail.com>
2022-08-05 15:24:45 +0200

    Added njit from numba to speed up loops

--- img2csd.py
+++ img2csd.py
@@ -4,8 +4,12 @@ import sys
 import argparse
 import os.path
 
+from numba import jit, njit
+
 
 from csdlib import csdlib as csd
+
+verbose = not True
 
 
 def myLog(s: str = "", *args, **kwargs):
@@ -13,8 +17,7 @@ def myLog(s: str = "", *args, **kwargs):
         print(s, args, kwargs)
 
 
-# Doing the main work here.
-if __name__ == "__main__":
+def getArgs():
 
     # 1 handling the in line parameters
     parser = argparse.ArgumentParser(
@@ -80,10 +83,77 @@ if __name__ == "__main__":
 
     parser.add_argument("image", help="Image input file")
     args = parser.parse_args()
-    config = vars(args)
 
-    verbose = config["verbose"]
-    myLog(config)
+    return vars(args)
+
+
+@njit
+def getCSD(array_img):
+    pixels_x = array_img.shape[0]
+    pixels_y = array_img.shape[1]
+
+    output_square_size = max(pixels_x, pixels_y)
+
+    XSecArray = np.zeros((output_square_size, output_square_size), dtype=np.int8)
+
+    for x in range(pixels_x):
+        for y in range(pixels_y):
+            result = 0
+
+            r = array_img[x, y, 0]
+            g = array_img[x, y, 1]
+            b = array_img[x, y, 2]
+
+            rgb = np.array((r, g, b))
+
+            if 25 < rgb.sum() < 3 * 255:
+                result = 1 + np.where(rgb == np.amax(rgb))[0][0]
+            XSecArray[x, y] = result
+
+    return XSecArray
+
+
+@njit
+def trimEmpty(XSecArray):
+
+    size_y, size_x = XSecArray.shape
+    crop_top = crop_btm = 0
+    crop_left = crop_right = 0
+
+    for row in range(size_y):
+        crop_top = row
+        if np.sum(XSecArray[row, :]) > 0:
+            break
+
+    for row in range(size_y):
+        crop_btm = row
+        if np.sum(XSecArray[-row - 1, :]) > 0:
+            break
+
+    for col in range(size_x):
+        crop_left = col
+        if np.sum(XSecArray[:, col]) > 0:
+            break
+
+    for col in range(size_x):
+        crop_right = col
+        if np.sum(XSecArray[:, -col - 1]) > 0:
+            break
+
+    new_size = 3 + max(size_x - crop_left - crop_right, size_y - crop_top - crop_btm)
+    # myLog(f"Size after cropping: {new_size}x{new_size}")
+    # myLog(f"Cropping: top {crop_top}\t btm {crop_btm}")
+    # myLog(f"Cropping: lft {crop_left}\t rgt {crop_right}")
+
+    crop_XSecArray = np.zeros((new_size, new_size))
+    crop_XSecArray[
+        1 : 1 + size_y - crop_top - crop_btm, 1 : 1 + size_x - crop_left - crop_right
+    ] = XSecArray[crop_top:-crop_btm, crop_left:-crop_right]
+
+    return crop_XSecArray
+
+
+def loadImageFromFile(config):
 
     source_img = config["image"]
     w = config["width"]
@@ -132,23 +202,55 @@ if __name__ == "__main__":
         # array_img = np.array(loaded_img)
         array_img = np.array(plt_img) * 255
 
-    myLog(array_img.shape)
+    return np.array(array_img)
 
+
+# @jit
+def simplify(XSecArray, dXmm, dYmm, maxsize):
+    myLog()
+    myLog("Simplifying the geometry...", end="")
+    splits = 1
+    for _ in range(10):
+        myLog(f"...{splits}", end="")
+        if dXmm < 1 or dYmm < 1 or max(XSecArray.shape) > maxsize:
+
+            XSecArray = XSecArray[::2, ::2]
+
+            dXmm = dXmm * 2
+            dYmm = dYmm * 2
+
+        else:
+            myLog()
+            myLog("No further simplification needed")
+            break
+
+    myLog()
+    myLog(f"dX:{dXmm}mm dY:{dYmm}mm")
+    myLog(f"Data table size: {XSecArray.shape}")
+    return XSecArray, dXmm, dYmm
+
+
+def main():
+    global verbose
+
+    config = getArgs()
+
+    verbose = config["verbose"]
+    source_img = config["image"]
+    w = config["width"]
+    h = config["height"]
+    optimize = config["optimize"]
+
+    myLog(config)
+
+    array_img = loadImageFromFile(config)
+    myLog(array_img.shape)
     pixels_x = array_img.shape[0]
     pixels_y = array_img.shape[1]
 
-    output_square_size = max(pixels_x, pixels_y)
+    dXmm = dYmm = min(w / pixels_x, h / pixels_y)
 
-    XSecArray = np.zeros((output_square_size, output_square_size))
-
-    for x in range(pixels_x):
-        for y in range(pixels_y):
-            result = 0
-            rgb = np.array(array_img[x, y])
-            if 25 < rgb.sum() < 3 * 255:
-                result = 1 + np.where(rgb == np.amax(rgb))[0][0]
-            XSecArray[x, y] = result
-
+    XSecArray = getCSD(array_img)
     myLog(XSecArray)
     myLog(XSecArray.shape)
 
@@ -156,69 +258,13 @@ if __name__ == "__main__":
         myLog("No cross section data found. No output generated.")
         sys.exit(1)
 
+    XSecArray = trimEmpty(XSecArray)
     myLog("Trimming the empty space...")
-    size_y, size_x = XSecArray.shape
-    crop_top = crop_btm = 0
-    crop_left = crop_right = 0
-
-    for row in range(size_y):
-        crop_top = row
-        if np.sum(XSecArray[row, :]) > 0:
-            break
-
-    for row in range(size_y):
-        crop_btm = row
-        if np.sum(XSecArray[-row - 1, :]) > 0:
-            break
-
-    for col in range(size_x):
-        crop_left = col
-        if np.sum(XSecArray[:, col]) > 0:
-            break
-
-    for col in range(size_x):
-        crop_right = col
-        if np.sum(XSecArray[:, -col - 1]) > 0:
-            break
-
-    new_size = 3 + max(size_x - crop_left - crop_right, size_y - crop_top - crop_btm)
-    myLog(f"Size after cropping: {new_size}x{new_size}")
-
-    myLog(f"Cropping: top {crop_top}\t btm {crop_btm}")
-    myLog(f"Cropping: lft {crop_left}\t rgt {crop_right}")
-
-    crop_XSecArray = np.zeros((new_size, new_size))
-    crop_XSecArray[
-        1 : 1 + size_y - crop_top - crop_btm, 1 : 1 + size_x - crop_left - crop_right
-    ] = XSecArray[crop_top:-crop_btm, crop_left:-crop_right]
-
-    XSecArray = crop_XSecArray
-
-    dXmm = dYmm = min(w / pixels_x, h / pixels_y)
-
+    myLog(XSecArray.shape)
     myLog(f"dX: {dXmm} mm,\t dY: {dYmm} mm")
 
-    if config["optimize"]:
-        myLog()
-        myLog("Simplifying the geometry...", end="")
-        splits = 1
-        for _ in range(4):
-            myLog(f"...{splits}", end="")
-            if dXmm < 1 or dYmm < 1 or max(XSecArray.shape) > config["maxsize"]:
-
-                XSecArray = XSecArray[::2, ::2]
-
-                dXmm = dXmm * 2
-                dYmm = dYmm * 2
-
-            else:
-                myLog()
-                myLog("No further simplification needed")
-                break
-
-        myLog()
-        myLog(f"dX:{dXmm}mm dY:{dYmm}mm")
-        myLog(f"Data table size: {XSecArray.shape}")
+    if optimize:
+        XSecArray, dXmm, dYmm = simplify(XSecArray, dXmm, dYmm, config["maxsize"])
 
     output_csd_file = source_img[:-4] + ".csd"
     dXmm = round(dXmm, 2)
@@ -226,3 +272,7 @@ if __name__ == "__main__":
 
     S = csd.cointainer(XSecArray, dXmm, dYmm)
     S.save(output_csd_file)
+
+
+if __name__ == "__main__":
+    main()
